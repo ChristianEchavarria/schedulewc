9321f8a71c5f637f663083a3289a8bbe88f2bb88
ChristianEchavarria <cristianechavarriaz@gmail.com>
2026-06-28 16:04:28 -0500

    Dieciseisavos cerrados: 03/07 8:30pm Colombia-Ghana

--- data.js
+++ data.js
@@ -115,7 +115,7 @@ window.WC_MATCHES = [
 
   { date: "Viernes, 3 de julio 2026", time: "13:00", team1: "Australia", team2: "Egipto", group: "Partido 86", stadium: "Estadio Miami", isKnockout: true },
   { date: "Viernes, 3 de julio 2026", time: "17:00", team1: "Argentina", team2: "Cabo Verde", group: "Partido 87", stadium: "Estadio Kansas City", isKnockout: true },
-  { date: "Viernes, 3 de julio 2026", time: "20:30", team1: "A definir", team2: "A definir", group: "Partido 88", stadium: "Estadio Dallas", isKnockout: true },
+  { date: "Viernes, 3 de julio 2026", time: "20:30", team1: "Colombia", team2: "Ghana", group: "Partido 88", stadium: "Estadio Dallas", isKnockout: true },
 
   { date: "Sábado, 4 de julio 2026", time: "15:00", team1: "Ganador Partido 74", team2: "Ganador Partido 77", group: "Partido 89", stadium: "Estadio Filadelfia", isKnockout: true },
   { date: "Sábado, 4 de julio 2026", time: "15:00", team1: "Ganador Partido 73", team2: "Ganador Partido 75", group: "Partido 90", stadium: "Estadio Houston", isKnockout: true },
